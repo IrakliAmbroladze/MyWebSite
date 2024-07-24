d6e63879726b97da9856faa359d3288a9810619d
IrakliAmbroladze <ambroladzeirakli@gmail.com>
2024-07-24 22:38:55 +0400

    script.js

--- script.js
+++ script.js
@@ -1,41 +1,20 @@
-document.getElementById('userForm').addEventListener('submit', async (event) => {
-  event.preventDefault();
+let slideIndex = 0;
+showSlides(slideIndex);
 
-  const name = document.getElementById('name').value;
-  const email = document.getElementById('email').value;
-
-  const response = await fetch('/api/users', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({ name, email })
-  });
-  fetchUsers();
-  // if (response.ok) {
-  //   alert('User added successfully');
-  //   fetchUsers(); // Refresh the user list
-  // } else {
-  //   alert('Failed to add user');
-  // }
-});
-
-async function fetchUsers() {
-  const response = await fetch('/api/users');
-  if (response.ok) {
-    const users = await response.json();
-
-    const userList = document.getElementById('userList');
-    userList.innerHTML = '';
-    users.forEach(user => {
-      const li = document.createElement('li');
-      li.textContent = `${user.name} - ${user.email}`;
-      userList.appendChild(li);
-    });
-  } else {
-    console.error('Failed to fetch users');
-  }
+function moveSlide(n) {
+    showSlides(slideIndex += n);
 }
 
-// Fetch and display users on page load
-fetchUsers();
+function showSlides(n) {
+    let slides = document.getElementsByClassName("slide");
+    if (n >= slides.length) {
+        slideIndex = 0;
+    }
+    if (n < 0) {
+        slideIndex = slides.length - 1;
+    }
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slides[slideIndex].style.display = "block";
+}
